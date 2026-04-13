9e568c02165798782fa83876b7dee93befd5f883
Ibrahim SECK <isk@geosys.local>
2026-04-13 17:42:51 +0200

    feature: retrieve row, col and box from grid

--- core/grid.py
+++ core/grid.py
@@ -7,7 +7,8 @@ import logging
 logging.basicConfig(
     level = logging.DEBUG,
     format = "{asctime}-{levelname}-{message}",
-    datefmt ="%Y-%m-%d %H:%M"
+    datefmt ="%Y-%m-%d %H:%M",
+    style="{"
 )
 logger = logging.getLogger(__name__)
 
@@ -46,7 +47,9 @@ class SudokuGrid:
     @grid.setter
     def grid(self, value):
         if not isinstance(value, list):
-            raise TypeError(f"{value} must be of type list")
+            raise TypeError(f"{value} must be of type array")
+        if any([elt<= self.GRID_SIZE  for elt in value]):
+            self._grid = value
 
 
     def validate_grid(self):
@@ -68,10 +71,22 @@ class SudokuGrid:
     def _get_box_size(self):
         box_size = math.sqrt(self.size)
         if box_size -int(box_size) ==0:
-            return box_size
+            return int(box_size)
         else:
             logger.warning("box size is not an integer. The sudodu grid is not standard")
             return NotImplemented
         
-    def _get_box(self):
-        ...
+    def _get_row(self, row_number):
+        return self.grid[row_number*self.size:(row_number+1)*self.size]
+    
+    def _get_col(self, col_number):
+        return [self._get_row(row)[col_number] for row in range(self.GRID_SIZE)]
+        
+    def _get_box(self, position =(0,0)):
+        box_size = self._get_box_size()
+        box_rows = [position[0]*box_size +row for row in range(box_size)]
+        box_cols = [position[1]*box_size +col for col in range(box_size)]
+        print(box_rows)
+        print('*'*10)
+        print(box_cols)
+        return [self._get_row(row)[col] for row in box_rows for col in box_cols]
